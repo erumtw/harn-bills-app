894bee5e6a7fa513db7f38dfd51262d9f87521bf
erumtw <heartof.effort@gmail.com>
2024-05-05 00:57:24 +0700

    update: get all bill api

--- src/api/constant/services.js
+++ src/api/constant/services.js
@@ -1,37 +1,51 @@
 // import {Alert} from 'react-native';
-import {User} from '../../constants/types';
-import {userEndpoint} from './api_endpoint';
+import { User } from '../../constants/types';
+import { groupEndpoint, userEndpoint } from './api_endpoint';
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const sign_in = async (username) => {
   try {
     const respone = await fetch(userEndpoint);
     const data = await respone.json();
-    // const user = data.find((user: User) => user.username === username);
+    const currentUser = data.find((user) => user.username === username);
 
-    // if (!user) {
-    //   console.log(username, 'not exist');
-    //   console.log('registering...');
+    if (!currentUser) {
+      console.log(username, 'not exist');
+      console.log('registering...');
 
-    //   const newUser: User = {
-    //     id: '',
-    //     username: username,
-    //     total_paid: 0
-    //   };
+      const newUser = {
+        id: '',
+        username: username,
+        total_paid: 0,
+      };
 
-    //   const postRes = await fetch(userEndpoint, {
-    //     method: 'post',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(newUser),
-    //   });
-    // }
+      const postRes = await fetch(userEndpoint, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      });
 
-    // return user;
+      return newUser;
+    }
+
+    return currentUser;
   } catch (error) {
     throw new Error(error.message);
   }
 
   //   console.log(user);
+};
+
+export const get_all_bills = async () => {
+  return await fetch(groupEndpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 };
