f61ce7c1937b4797cf732c8ed6e561755bf7df8b
erumtw <heartof.effort@gmail.com>
2024-05-05 00:56:44 +0700

    add assets

---
+++ src/constants/icons.js
@@ -0,0 +1,13 @@
+import home from "../assets/icons/home.png";
+import plus from "../assets/icons/plus.png";
+import profile from "../assets/icons/profile.png";
+import division from "../assets/icons/division.png";
+import plus_2 from "../assets/icons/plus_2.png";
+
+export default {
+  home,
+  plus,
+  plus_2,
+  division,
+  profile,
+};
