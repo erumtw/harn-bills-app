0bc1b7bce77c7edea448b2f53b0fa357265000e8
erumtw <heartof.effort@gmail.com>
2024-05-05 00:54:56 +0700

    finish user login

--- src/screens/(auth)/_layout.jsx
+++ src/screens/(auth)/_layout.jsx
@@ -1,7 +1,7 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignIn from './sign-in';
 // import SignUp from './sign-up.test';
 
@@ -9,10 +9,16 @@ const AuthStack = createNativeStackNavigator();
 
 const AuthLayout = () => {
   return (
+    <>
       <AuthStack.Navigator>
-         <AuthStack.Screen name='sign-in' component={SignIn} options={{headerShown: false}}/>
-         {/* <Stack.Screen name='sign-up' component={SignUp} options={{headerShown: false}}/> */}
+        <AuthStack.Screen
+          name="sign-in"
+          component={SignIn}
+          options={{headerShown: false}}
+        />
+        {/* <Stack.Screen name='sign-up' component={SignUp} options={{headerShown: false}}/> */}
       </AuthStack.Navigator>
+    </>
   );
 };
 
--- src/screens/(auth)/sign-in.jsx
+++ src/screens/(auth)/sign-in.jsx
@@ -1,19 +1,15 @@
-import {View, Text, ScrollView, TextInput, Alert} from 'react-native';
-import React, {useState} from 'react';
-// import FormField from '../../components/FormField.js';
-// import CustomButton from '../../components/CustomButton.js';
+import { View, Text, ScrollView, TextInput, Alert, Image } from 'react-native';
+import React, { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// import {userEndpoint} from '../../api/constant/api_endpoint.js';
-// import {User} from '../../constants/types.js';
-import {sign_in} from '../../api/constant/services.js';
-import {useNavigation} from '@react-navigation/native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { sign_in } from '../../api/constant/services.js';
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import FormField from '../../components/FormField.jsx';
 import CustomButton from '../../components/CustomButton.jsx';
+import icons from '../../constants/icons.js';
 // import { useGlobalContext } from "../../context/GlobalProvider.js";
 
-const SignIn = () => {
-  const navigation = useNavigation();
+const SignIn = ({ navigation }) => {
   // const { isLoggedIn } = useGlobalContext();
   const [submit, setSubmit] = useState(false);
   const [form, setForm] = useState({
@@ -35,10 +31,10 @@ const SignIn = () => {
 
     try {
       // check authentication if right navigate to home else throw error
-      // const user = await sign_in(form.username);
-      // await AsyncStorage.setItem('user', user);
+      const user = await sign_in(form.username);
+      await AsyncStorage.setItem('user', JSON.stringify(user));
       // console.log(JSON.parse(await AsyncStorage.getItem("user")));
-      // navigation.navigate("home");
+      navigation.replace('(tabs)', { screen: 'home' });
     } catch (error) {
       Alert.alert('Invalid Sign In', error.message);
     } finally {
@@ -50,30 +46,30 @@ const SignIn = () => {
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
         <View className="justify-center px-5">
-          <View className="w-full mt-32">
+          <View className="w-full mt-[25vh]">
             <View className="flex-row justify-start mb-5">
-              {/* <Feather
-                name="divide"
-                color="#FF9C01"
-                size={28}
-                className="mr-1"
-              /> */}
-              <Text className="text-5xl text-secondary-100  ml-1">
+              <Image
+                source={icons.division}
+                className="w-10 h-10"
+                resizeMode="contain"
+                tintColor="#FF9C01"
+              />
+              <Text className="text-5xl text-secondary-100 font-bold ml-2">
                 HarnBill
               </Text>
             </View>
-            <Text className="text-white text-2xl font-pregular">Sign In</Text>
+            <Text className="text-white text-2xl font-medium">Sign In</Text>
           </View>
 
           <FormField
             title="username"
-            handleChange={e => setForm({...form, username: e})}
+            handleChange={(e) => setForm({ ...form, username: e })}
             value={form.username}
             otherStyles="mt-5"
           />
           <CustomButton
             title="Sign In"
-            containerStyles="mt-8"
+            containerStyles="mt-8 items-center"
             handlePress={onSubmit}
           />
 
